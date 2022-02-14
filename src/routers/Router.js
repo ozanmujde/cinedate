8c03f7d5f8e7beccbe9ec8081c483f00b71c340b
Ozan Müjde <ozanmujde@googlemail.com>
2022-02-14 20:00:36 +0300

    Bottom tab icons added
    icons are added for bottom tab but no additional styling

--- src/routers/Router.js
+++ src/routers/Router.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet } from "react-native";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import LoginScreen from "../screens/LoginScreen";
@@ -12,6 +13,7 @@ import SearchScreen from "../screens/SearchScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import InboxScreen from "../screens/InboxScreen";
 import SetScreen from "../screens/SetScreen";
+import { Ionicons } from "@expo/vector-icons";
 import {
   Provider as AuthProvider,
   Context as AuthContext,
@@ -40,16 +42,98 @@ const router = () => {
           <Stack.Screen name="Flipcard" component={FlipcardComponent} />
         </Stack.Navigator>
       ) : (
-        <Tab.Navigator initialRouteName="Home">
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Set" component={SetScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-          <Tab.Screen name="Inbox" component={InboxScreen} />
+        <Tab.Navigator
+          initialRouteName="Home"
+          // screenOptions={{
+          //   tabBarOptions: {
+          //     activeTintColor: "tomato",
+          //     inactiveTintColor: "gray",
+          //   },
+          //   style: {
+          //     ...styles.shadow,
+          //   },
+          // }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="ios-home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={SearchScreen}
+            options={{
+              tabBarLabel: "Search",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="ios-search" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Set"
+            component={SetScreen}
+            options={{
+              tabBarLabel: "Set",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="add-circle-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Inbox"
+            component={InboxScreen}
+            options={{
+              tabBarLabel: "Inbox",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="ios-mail" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons
+                  name="person-circle-outline"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
         </Tab.Navigator>
       )}
     </NavigationContainer>
   );
+
+  //     >
+  //       <Tab.Screen name="Home" component={HomeScreen} />
+  //       <Tab.Screen name="Search" component={SearchScreen} />
+  //       <Tab.Screen name="Set" component={SetScreen} />
+  //       <Tab.Screen name="Profile" component={ProfileScreen} />
+  //       <Tab.Screen name="Inbox" component={InboxScreen} />
+  //     </Tab.Navigator>
+  //   )}
+  // </NavigationContainer>
+  // );
 };
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 3.5,
+    elevation: 5,
+    borderRadius: 10,
+  },
+});
 
 export default router;
