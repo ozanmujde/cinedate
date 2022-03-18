6d1a56828c82101fa190bf0ca31916e3b038bfbf
Ozan Müjde <ozanmujde@googlemail.com>
2022-03-18 19:10:08 +0300

    Hotfix
    Component modernized

--- src/Components/ChatComponents/ChatComponent.js
+++ src/Components/ChatComponents/ChatComponent.js
@@ -1,38 +1,37 @@
-import { GiftedChat } from "react-native-gifted-chat";
-import React, { Component } from "react";
-import Users from "../../../assets/Users";
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-export default class ChatComponent extends Component {
-  state = { messages: [] };
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: Users[0].imageUri,
-          },
+export function ChatComponent() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
         },
-      ],
-    });
-  }
+      },
+    ])
+  }, [])
 
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
-  render() {
-    return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={(messages) => this.onSend(messages)}
-        user={{ _id: 1 }}
-      />
-    );
-  }
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
+
+export default ChatComponent;
